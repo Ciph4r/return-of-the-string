8fe6944642f965f84669209a410b62f9ce17a6c6
David Lau <David.lau@codeimmersives.com>
2020-02-02 17:57:25 -0500

    finished

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 /*******************
  * YOUR CODE HERE! *
  *******************/
+capitalize =(cap) => {
+  let captial = cap.toUpperCase();
+  return captial;
+};
 
 
+exclaim = (word) => {
+let newWord = word + '!';
+return newWord;
+};
 
+firstCharacter = (first) => {
+let fI = first.charAt(0);
+return fI;
+};
 
+lastCharacter = (last) => {
+  let lI = last.charAt(last.length-1);
+  return lI;
+};
+
+oneCharacter = (word,num) => {
+let newChar = word.charAt(num);
+return newChar;
+}
  
+twoCharacters = (word,num,para) => {
+let newWord = word.charAt(num) + word.charAt(para);
+return newWord;
+};
+
+yeller = (word) => {
+  let newWord = word.toUpperCase() +'!!!';
+  return newWord;
+};
+
+initials = (name) => {
+  let newName = name.charAt(0) + '.' + name.charAt(name.indexOf(' ')+1) + '.'
+  return newName
+}
+
+
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
